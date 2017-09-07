d51233bd0863d3dd3b382f4cad50b410d6d7657a
Marcos Gin <marcosgin291@gmail.com>
2017-09-07 02:38:26 -0300

    Use the service users for the *ngFor in the table list of users

--- src/app/components/users/list/user-list.component.ts
+++ src/app/components/users/list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService, Users } from '../../../services/users.service';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  listUsers: Users[] = [];
+
+  constructor( private _usersService: UsersService) { }
 
   ngOnInit() {
+    this.listUsers = this._usersService.getUsers();
   }
 
 }
