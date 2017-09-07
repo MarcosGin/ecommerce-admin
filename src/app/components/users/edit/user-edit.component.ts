af65447346ee1f80bba42f248143fecf07064641
Marcos Gin <marcosgin291@gmail.com>
2017-09-07 03:11:46 -0300

    We retrieve the user id that comes from the url, and we use the service to obtain the user data, then we generate the form with the data (ngForm)

--- src/app/components/users/edit/user-edit.component.ts
+++ src/app/components/users/edit/user-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsersService, Users } from '../../../services/users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  user: Users;
+  id: number;
+  constructor( private _usersService: UsersService,
+               private router: ActivatedRoute ) {
+    this.router.params.subscribe( params => {
+        this.id = params['id'];
+    });
   }
 
+  ngOnInit() {
+    this.user = this._usersService.getUser(this.id);
+  }
+
+  saveChanges () {
+
+  }
 }
