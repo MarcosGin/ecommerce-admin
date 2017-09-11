60341df0f0e98b1f16f94d989c75f762066efbe9
Marcos Gin <marcosgin291@gmail.com>
2017-09-11 19:00:39 -0300

    Interface for the products

---
+++ src/app/interfaces/products.ts
@@ -0,0 +1,11 @@
+export interface Products {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  mark: string;
+  price: number;
+  stock: number;
+  created_at: string;
+  updated_at: string;
+}
