d3827c2ff6ab365fa651cf9158b603f1ba289753
Marcos Gin <marcosgin291@gmail.com>
2017-09-19 02:22:40 -0300

    Add the paths in the routes for the marks and categories

--- src/app/admin/components/products/products.routes.ts
+++ src/app/admin/components/products/products.routes.ts
@@ -3,10 +3,14 @@ import { Routes } from '@angular/router';
 import { ProductEditComponent } from './edit/product-edit.component';
 import { ProductListComponent } from './list/product-list.component';
 import { ProductAddComponent } from './add/product-add.component';
+import { MarksComponent } from './marks/marks.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 export const PRODUCTS_ROUTES: Routes = [
   { path: '', component: ProductListComponent},
   { path: 'edit/:id', component: ProductEditComponent},
   { path: 'add', component: ProductAddComponent},
+  { path: 'marks/add', component: MarksComponent },
+  { path: 'categories/add', component: CategoriesComponent},
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
