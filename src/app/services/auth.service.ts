9cc8f41ce3942ba50adb64abc194d4f9c06ed05f
Marcos Gin <marcosgin291@gmail.com>
2017-09-19 16:45:49 -0300

    Using the api, and send the login, return the json if the result if true create in the localstore he jwt;

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,40 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { environment } from '../../environments/environment';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
 
-  private users = [] = [
-    {
-      id: 1,
-      username: 'admin',
-      password: 'admin1'
-    },
-    {
-      id: 2,
-      username: 'Marcos',
-      password: 'godkhyzo10'
-    }
-  ];
+  public token: string;
 
-  public user_id: number;
-
-  constructor () {
+  constructor ( private http: Http) {
+    const currentUser = JSON.parse(localStorage.getItem('jwt'));
+    this.token = currentUser && currentUser.jwt;
   }
 
-  login( username: string, password: string ): boolean {
-    let result = false;
-    for (const user of this.users) {
-      if (user.username === username && user.password === password) {
-        localStorage.setItem('jwt', JSON.stringify({token: 'notoken', user_id: user.id}));
-        result = true;
-      }
-    }
-    return result;
-  }
-
-  getUser() {
-    this.user_id = JSON.parse(localStorage.getItem('jwt')).user_id;
-    return this.user_id;
+  login( username: string, password: string ) {
+    const apiUrl = environment.apiUrl + 'account/login';
+    return this.http.post(apiUrl, JSON.stringify({email: username, password: password}))
+      .map( res => {
+        if (res.json().result === true) {
+          this.token = res.json().jwt;
+          localStorage.setItem('jwt', JSON.stringify({jwt: res.json().jwt }));
+        }
+        return res.json();
+      });
   }
 
   logout(): void {
