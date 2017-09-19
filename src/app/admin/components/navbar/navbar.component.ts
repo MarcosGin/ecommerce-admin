e937caaa38a16705a30a6887470ee6fab656f301
Marcos Gin <marcosgin291@gmail.com>
2017-09-19 03:28:03 -0300

    Use an function for get the title of the section, using the path of the window

--- src/app/admin/components/navbar/navbar.component.ts
+++ src/app/admin/components/navbar/navbar.component.ts
@@ -9,10 +9,38 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
+  private listRoutes: any = [
+    {path: '', title: 'Home'},
+    {path: 'home', title: 'Home'},
+    {path: 'users', title: 'Users'},
+    {path: 'products', title: 'Products'}
+  ];
+
   constructor( private _authService: AuthService,
                private router: Router ) { }
 
   ngOnInit() {
+  }
+
+  getTittle() {
+    let titlee = window.location.pathname;
+    function SplitTheString(ResultStr) {
+      if (ResultStr != null) {
+        const SplitChars = '/';
+        if (ResultStr.indexOf(SplitChars) >= 0) {
+          return ResultStr.split(SplitChars);
+        }
+      }
+    }
+    titlee = SplitTheString(titlee);
+    if (typeof titlee[2] === 'undefined') {
+      titlee[2] = 'home';
+    }
+    for (const routes of this.listRoutes) {
+      if (routes.path === titlee[2]) {
+          return routes.title;
+      }
+    }
   }
 
   logout() {
