3788037230a42b9ec4f18e244fba71c308fac87f
Marcos Gin <marcosgin291@gmail.com>
2017-09-28 08:56:41 -0300

    remove the data necessary

--- src/app/admin/components/account/account.component.ts
+++ src/app/admin/components/account/account.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Users } from '../../../services/users.service';
-import { AccountService } from '../../../services/account.service';
 
 @Component({
   selector: 'app-account',
@@ -9,15 +7,8 @@ import { AccountService } from '../../../services/account.service';
 })
 export class AccountComponent implements OnInit {
 
-  public user: Users;
 
-  constructor( private _accountService: AccountService) {
-    this.user = new Users();
-   this._accountService.getProfile()
-     .subscribe( data => {
-     this.user = data;
-   });
-  }
+  constructor() {}
 
   ngOnInit() {
   }
