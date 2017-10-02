9d94043265d4cdfd34d8666ab50826abdae9a2b4
Marcos Gin <marcosgin291@gmail.com>
2017-10-02 06:46:45 -0300

    new component for the sidebar responsive with its html structure

---
+++ src/app/admin/components/sidebar/sidebar-responsive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-responsive',
+  templateUrl: './sidebar-responsive.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarResponsiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
