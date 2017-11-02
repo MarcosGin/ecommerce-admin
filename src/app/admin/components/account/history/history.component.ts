a709b047c1c43030cbccbf33a9289ae97991aabb
Marcos Gin <marcosgin291@gmail.com>
2017-11-02 17:28:15 -0300

    pagination for the history, and new styles for the table

--- src/app/admin/components/account/history/history.component.ts
+++ src/app/admin/components/account/history/history.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../../../../services/account.service';
+import { PaginationService } from '../../../../services/pagination.service';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styles: []
+  styleUrls: ['../account.component.css']
 })
 export class HistoryComponent implements OnInit {
 
   public historySessions = [];
-  constructor(private _accountService: AccountService) { }
+  public pagerHistories: any = {};
+  public pagedHistories: any[];
+  constructor(private _accountService: AccountService,
+              private _paginationService: PaginationService) { }
 
   ngOnInit() {
     this._accountService.getHistory()
       .subscribe(data => {
        this.historySessions = data.response.sessions;
+       this.setPageHistories(1);
       });
+  }
+
+  setPageHistories(page: number) {
+    if (page < 1 || page > this.pagerHistories.totalPages) {
+      return;
+    }
+
+    this.pagerHistories = this._paginationService.getPager(this.historySessions.length, page, 5);
+    this.pagedHistories = this.historySessions.slice(this.pagerHistories.startIndex, this.pagerHistories.endIndex + 1);
   }
 
 }
